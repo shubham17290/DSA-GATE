15ddff8b7825583c7107ad5839c1a2b26c8da7b8
Gian <smourya1046@gmail.com>
2026-08-08 15:32:46 +0530

    addressing  way in the Multi dimensional array

--- 01 Introduction/lec 05/01 Codes/03 3-D array/example_02.c
+++ 01 Introduction/lec 05/01 Codes/03 3-D array/example_02.c
@@ -3,9 +3,9 @@
 int main() {
     //  checking the address concept
     int a[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    printf("%u\n", a);
-    printf("%u\n", a[0]);
-    printf("%u\n", &a);
+    printf("%u\n", a + 1);
+    printf("%u\n", a[0] + 1);
+    printf("%u\n", &a + 1);
 
     return 0;
 }
